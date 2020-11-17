d797ec6144be916c01b8622d9327a5dd36104a51
Michael <michaelnguyen974@gmail.com>
2020-11-17 20:09:25 +0000

    Added useState in component to change colour when toggling

--- my-toggle-switch/src/App.js
+++ my-toggle-switch/src/App.js
@@ -1,10 +1,17 @@
+import React, { useState } from "react";
 import "./styles.css";
 import Switch from "../src/components/Switch";
 
 const App = () => {
+  const [isToggled, setIsToggled] = useState(false); //creates state in the component
+  
   return (
     <div className="App">
-      <Switch rounded={true}/>
+      <Switch
+        rounded={true}
+        isToggled={isToggled} // isToggled is =false from useState by default
+        onToggle={() => setIsToggled(!isToggled)} // onToggle changes when input changes, sets to true
+      />
     </div>
   );
 };
--- my-toggle-switch/src/components/Switch.js
+++ my-toggle-switch/src/components/Switch.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./Switch.css";
 import cx from "classnames";
 
-const Switch = ({ rounded = false }) => {
+const Switch = ({ rounded = false, isToggled, onToggle }) => {
   const sliderCX = cx("slider", {
     rounded: rounded,
   });
 
+  // isToggled is the value of the input, onToggle is when the input changes
   return (
     <label className="switch">
-      <input type="checkBox" />
+      <input type="checkBox" checked={isToggled} onChange={onToggle} /> 
       <span className={sliderCX} />
     </label>
   );
